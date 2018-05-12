f1ae1251d09f0045a8c665c364333bf0ded3c3e9
Mohamed Boudra <boudra.moha@gmail.com>
2018-05-12 21:03:49 +0200

    implement decode binary to term interface

--- lib/jaxon.ex
+++ lib/jaxon.ex
@@ -1,71 +1,74 @@
 defmodule Jaxon do
-  def do_decode(d, {:key, key}, acc, path) do
-    do_decode(d, Jaxon.Decoder.decode(d), acc, [key | path])
+  def do_decode([{:key, key} | rest], acc, path) do
+    do_decode(rest, acc, [key | path])
   end
 
-  def do_decode(d, :start_array, acc, path) do
-    do_decode(d, Jaxon.Decoder.decode(d), [[] | acc], path)
+  def do_decode([:start_array | rest], acc, path) do
+    do_decode(rest, [[] | acc], path)
   end
 
-  def do_decode(d, :start_object, acc, path) do
-    do_decode(d, Jaxon.Decoder.decode(d), [%{} | acc], path)
+  def do_decode([:start_object | rest], acc, path) do
+    do_decode(rest, [%{} | acc], path)
   end
 
-  def do_decode(_, _, [last], _) do
-    {:ok, last}
+  def do_decode([:end], [acc], _) do
+    {:ok, acc}
   end
 
-  def do_decode(_, {:string, value}, [], _) do
-    {:ok, value}
+  def do_decode([{:incomplete, _} | _], _, _) do
+    {:error, ""}
   end
 
-  def do_decode(_, {:integer, value}, [], _) do
-    {:ok, value}
-  end
-
-  def do_decode(_, {:decimal, value}, [], _) do
-    {:ok, value}
-  end
-
-  def do_decode(_, {:boolean, value}, [], _) do
-    {:ok, value}
-  end
-
-  def do_decode(d, event, [value, parent | acc], [key | path])
+  def do_decode([event | rest], [value, parent | acc], [key | path])
       when event in [:end_array, :end_object] and is_map(parent) do
-    do_decode(d, Jaxon.Decoder.decode(d), [Map.put(parent, key, value) | acc], path)
+    do_decode(rest, [Map.put(parent, key, value) | acc], path)
   end
 
-  def do_decode(d, event, [value, parent | acc], path)
+  def do_decode([event | rest], [value, parent | acc], path)
       when event in [:end_array, :end_object] and is_list(parent) do
-    do_decode(d, Jaxon.Decoder.decode(d), [parent ++ [value] | acc], path)
+    do_decode(rest, [parent ++ [value] | acc], path)
   end
 
-  def do_decode(d, nil, [%{} = parent | acc], [key | path]) do
-    do_decode(d, Jaxon.Decoder.decode(d), [Map.put(parent, key, nil) | acc], path)
+  def do_decode([:end_object | rest], acc, path) do
+    do_decode(rest, acc, path)
   end
 
-  def do_decode(d, value = nil, [parent | acc], path) do
-    do_decode(d, Jaxon.Decoder.decode(d), [parent ++ [value] | acc], path)
+  def do_decode([:end_array | rest], acc, path) do
+    do_decode(rest, acc, path)
   end
 
-  def do_decode(d, {event, value}, [parent | acc], [key | path])
+  def do_decode([nil | rest], [%{} = parent | acc], [key | path]) do
+    do_decode(rest, [Map.put(parent, key, nil) | acc], path)
+  end
+
+  def do_decode([value = nil | rest], [parent | acc], path) do
+    do_decode(rest, [parent ++ [value] | acc], path)
+  end
+
+  def do_decode([{event, value} | rest], [parent | acc], [key | path])
       when event in [:string, :integer, :decimal, :boolean] and is_map(parent) do
-    do_decode(d, Jaxon.Decoder.decode(d), [Map.put(parent, key, value) | acc], path)
+    do_decode(rest, [Map.put(parent, key, value) | acc], path)
   end
 
-  def do_decode(d, {event, value}, [parent | acc], path)
+  def do_decode([{event, value} | rest], [parent | acc], path)
       when event in [:string, :integer, :decimal, :boolean] and is_list(parent) do
-    do_decode(d, Jaxon.Decoder.decode(d), [parent ++ [value] | acc], path)
+    do_decode(rest, [parent ++ [value] | acc], path)
+  end
+
+  def do_decode([{event, value} | rest], acc, path)
+      when event in [:string, :integer, :decimal, :boolean] do
+    do_decode(rest, [value | acc], path)
   end
 
   def decode(binary) do
-    d = Jaxon.Decoder.new() |> Jaxon.Decoder.update(binary)
-
-    do_decode(d, Jaxon.Decoder.decode(d), [], [])
+    binary
+    |> Jaxon.Decoder.decode_binary()
+    |> do_decode([], [])
   end
 
   def decode!(binary) do
-    decode(binary)
+    case decode(binary) do
+      {:ok, term} -> term
+    end
   end
 end
