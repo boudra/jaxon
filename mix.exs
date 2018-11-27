38fe2fa57d7a7ba8c1c62318839a9e78c1f3efae
Mohamed Boudra <boudra.moha@gmail.com>
2018-11-27 22:11:16 +0100

    chore: bump to 1.0.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Jaxon.MixProject do
     [
       app: :jaxon,
       name: "Jaxon",
-      version: "1.0.5",
+      version: "1.0.6",
       elixir: "~> 1.6",
       compilers: [:elixir_make] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
