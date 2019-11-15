1683a9992b2f5ce4093bcf10e9f74699a6245f19
Mohamed Boudra <boudra.moha@gmail.com>
2019-11-15 22:51:36 +0100

    feat: elixir 1.4

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Jaxon.MixProject do
       app: :jaxon,
       name: "Jaxon",
       version: "1.0.10",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       compilers: [:elixir_make] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
