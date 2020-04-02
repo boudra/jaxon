12887e724d1d66217be48e1f39cc7d55c60a3907
Mohamed Boudra <boudra.moha@gmail.com>
2020-04-02 20:44:34 +0200

    chore: set version to 2.0 alpha1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Jaxon.MixProject do
     [
       app: :jaxon,
       name: "Jaxon",
-      version: "1.0.10",
+      version: "2.0.0-alpha.1",
       elixir: "~> 1.7",
       compilers: [:elixir_make] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
