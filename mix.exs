cc6e967b54847076d0d190e8f971ce9e80f1ce75
Mohamed Boudra <boudra.moha@gmail.com>
2021-05-31 10:04:58 +0200

    version: bump to 2.0.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Jaxon.MixProject do
     [
       app: :jaxon,
       name: "Jaxon",
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.7",
       compilers: [:elixir_make] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
